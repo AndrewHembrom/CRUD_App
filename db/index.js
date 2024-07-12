f2631acde5dac3852287aff0595ac5de10052072
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-07-12 23:53:48 +0530

    structuring the webapp

---
+++ db/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+//Connect to MongoDB
+mongoose.connect('');
+
+//Define Schemas
+const AdminSchema = new mongoose.Schema({
+
+});
+
+const UserSchema = new mongoose.Schema({
+
+});
+
+const CourseSchema = new mongoose.Schema({
+    
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
